5d8e1916c1ebf0ef5d987164c7b97c0f3382a3a2
IRJ <jirenerose@gmail.com>
2022-09-28 10:43:12 +0530

    update

--- homeservice/settings.py
+++ homeservice/settings.py
@@ -20,10 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_5^9ta=%8rfkt#06n=#i_^9@^*os)^+8dru)#z7p!9&(jw5huy'
+# SECRET_KEY = 'django-insecure-_5^9ta=%8rfkt#06n=#i_^9@^*os)^+8dru)#z7p!9&(jw5huy'
+# import os
+# SECRET_KEY = os.environ['SECRET_KEY']
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
+export DJANGO_DEBUG=False
 
 ALLOWED_HOSTS = []
 
